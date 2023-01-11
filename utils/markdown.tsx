b4f77384d9c4cd4b03908d5ca11ba33a80abee12
Noah Buscher <me@noahbuscher.com>
2023-01-10 20:33:24 -0800

    fix link import

--- utils/markdown.tsx
+++ utils/markdown.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Link from "next/Link";
+import Link from "next/link";
 import * as ReactDOMServer from "react-dom/server";
 import ReactMarkdown from "react-markdown";
 
