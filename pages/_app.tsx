fda3d179e7f3279ce26b6681a74e55e21be3f2d9
Noah Buscher <me@noahbuscher.com>
2023-01-11 07:53:01 -0800

    add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,35 @@
 import "../styles/globals.css";
+
 import type { AppProps } from "next/app";
+import Script from "next/script";
+
 import { AnimatePresence } from "framer-motion";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <AnimatePresence mode="wait" initial={false}>
-      <Component {...pageProps} />
-    </AnimatePresence>
+    <>
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-HFLRPM9H0F"
+      />
+          
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-HFLRPM9H0F', {
+                page_path: window.location.pathname,
+              });
+            `,
+        }}
+      />
+      <AnimatePresence mode="wait" initial={false}>
+        <Component {...pageProps} />
+      </AnimatePresence>
+    </>
   );
 }
